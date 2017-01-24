32e382539eaa9c65f83afb7fb041e905b02e90e4
Daniel Martinez <dmartz86@gmail.com>
2017-01-23 21:31:55 -0500

    initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FilterComponent } from './filter/filter.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeadComponent } from './head/head.component';
+import { SliderComponent } from './slider/slider.component';
+import { FooterComponent } from './footer/footer.component';
+import { ActionsComponent } from './actions/actions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterComponent,
+    MenuComponent,
+    HeadComponent,
+    SliderComponent,
+    FooterComponent,
+    ActionsComponent
   ],
   imports: [
     BrowserModule,
